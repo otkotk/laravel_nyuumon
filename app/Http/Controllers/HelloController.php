2c6821412677a2d4681c1e5048769a20822148cb
gushikawa_syokuren_ota <ser1ous.bo.36dm@gmail.com>
2020-08-26 11:11:25 +0900

    Laravel入門の環境構築

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index(Request $request){
+        $data = [
+            'msg'=>'これはBladeを利用したサンプルです。'
+        ];
+        return view('hello.index', $data);
+    }
+    // public function index(Request $request, Response $response){
+    //     $req_url = $request->url();
+    //     $req_fullUrl = $request->fullUrl();
+    //     $req_path = $request->path();
+    //     $html = <<<EOF
+    //     <html>
+    //     <head>
+    //         <title>Hello/Index</title>
+    //         <style>
+    //         body{
+    //             font-size: 16pt;
+    //             color: #999;
+    //         }
+    //         h1{
+    //             font-size: 120pt;
+    //             text-align: right;
+    //             color: #fafafa;
+    //             margin: -50px 0 -120px 0;
+    //         }
+    //         </style>
+    //     </head>
+    //     <body>
+    //         <h1>Hello</h1>
+    //         <h3>Request</h3>
+    //         <pre>{$request}</pre>
+    //         <pre>$req_url</pre>
+    //         <pre>$req_fullUrl</pre>
+    //         <pre>$req_path</pre>
+    //         <h3>Response</h3>
+    //         <pre>{$response}</pre>
+    //         <pre>$req_path</pre>
+    //     </body>
+    //     </html>
+    //     EOF;
+
+    //     $response->setContent($html);
+    //     return $response;
+    // }
+}
+
+// global $head, $style, $body, $end;
+
+// $head = '<html><head>';
+// $style = <<<EOF
+//     <style>
+//         body { font-size:16pt; color:#999; }
+//         h1 { font-size:100pt; text-align:right; color:#eee;
+//             margin:-40 0 -50px 0; }
+//     </style>
+//     EOF;
+// $body = '</head><body>';
+// $end = '</body></html>';
+
+// function tag($tag, $txt){
+//     return "<{$tag}>" . $txt . "</{$tag}>";
+// }
+
+// class HelloController extends Controller
+// {
+//     public function __invoke()
+//     {
+//         return <<<EOF
+//         <html>
+//         <head>
+//             <title>Hello</title>
+//             <style>
+//                 body {
+//                     font-size:16pt; color:#999;
+//                 }
+//                 h1 {
+//                     font-size:30pt;
+//                     text-align:right;
+//                     color:#eee;
+//                     margin:-15px 0 0 0;
+//                 }
+//             </style>
+//         <body>
+//             <h1>Single Action</h1>
+//             <p>これは、シングルアクションコントローラーのアクションでし。</p>
+//         </body>
+//         </head>
+//         </html>
+//         EOF;
+//     }
+
+//     // public function index(){
+//     //     global $head, $style, $body, $end;
+
+//     //     $html = $head . tag('title', 'Hello/Index') . $style .
+//     //         $body
+//     //         . tag('h1', 'Index') . tag('p', 'I have a pen')
+//     //         . '<a href="/hello/other">go to Other page</a>'
+//     //         . $end;
+//     //     return $html;
+//     // }
+
+//     // public function other(){
+//     //     global $head, $style, $body, $end;
+//     //     $html = $head . tag('title', 'Hello/Other') . $style .
+//     //         $body
+//     //         . tag('h1', 'Other') . tag('p', 'I have an apple')
+//     //         . $end;
+//     //     return $html;
+//     // }
+// }
---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// $html = <<<EOF
+// <html>
+// <head>
+//     <title>HELLO.</title>
+//     <style>
+//         body{font-size:16pt; color:#999;}
+//         h1{font-size:100pt; text-align:right; color:#eee; margin:-40px 0 -50px 0px;}
+//     </style>
+// </head>
+// <body>
+//     <h1>HELLOOO.lol</h1>
+//     <p>I have a pen.</p>
+//     <p>I have a apple...</p>
+//     <p>アッポーペン</p>
+// </body>
+// </html>
+// EOF;
+
+// Route::get('hello/{msg?}', function($msg='?'){
+//     $html = <<<EOF
+//     <html>
+//     <head>
+//         <title>HELLO.^^/</title>
+//         <style>
+//             body{ font-size:16pt; color:#999; }
+//             h1{ font-size:100pt; text-align:right; color:#eee; margin:-40px 0 -50px 0; }
+//         </style>
+//     </head>
+//     <body>
+//         <h1>HELLO</h1>
+//         <p>hello/? 「?」に入力した値がここに出ます ☞ {$msg}</p>
+//         <p>これは、Route::get() の中に書いてます。</p>
+//     </body>
+//     </html>
+// EOF;
+//     return $html;
+// });
+
+// Route::get('hello/{id?}/{pass?}', 'HelloController@index');
+
+// Route::get('hello', 'HelloController@index');
+// Route::get('hello/other', 'HelloController@other');
+// Route::get('hello', 'HelloController');
+// Route::get('hello', 'HelloController@index');
+
+// Route::get('hello', function(){
+//     return view('hello.index');
+// });
+
+Route::get('hello', 'HelloController@index');
